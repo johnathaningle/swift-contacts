8027c953e0f60d0f7e43f4a09342d57afe99ceeb
John Ingle <jingle1000@gmail.com>
2019-03-29 16:16:18 -0400

    Update express server depenencies

--- node-backend/app.js
+++ node-backend/app.js
@@ -5,23 +5,34 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 const nodemailer = require('nodemailer');
 const cors = require("cors");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 //create the app
 const app = express();
 //dynmaically assign the port
 const port = process.env.port || 5000;
 
-//Routes for the web API
-app.use('/', require('./routes/index'));
-app.use('/account/', require('./routes/users'));
-
 //Extensions for the web server
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+app.use(morgan('combined'));
+app.use(bodyParser.json());
 
+//Routes for the web API
+app.use('/', require('./routes/index'));
+app.use('/account/', require('./routes/users'));
 
+//database setup
+const sequelize = new Sequelize("mysql://johnathaningle:P@$$w0rd@localhost:3306/swift_contacts");
 
+sequelize.authenticate().then(() => {
+    console.log("connection established");
+}).catch(err => {
+    console.log("couldnt connect to the database");
+});
 
 //Run the web server
 app.listen(port, () => {
