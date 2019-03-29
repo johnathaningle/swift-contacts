a975915b5bc326168fdbb4ae00f962d1b7455536
John Ingle <jingle1000@gmail.com>
2019-03-29 17:02:11 -0400

    Update vuex data store

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    username: "",
+    password: "",
   },
   mutations: {
-
+    loginUser(state) {
+      let x = state;
+      x.toString();
+    }
   },
   actions: {
+    loginUser(context) {
+      context.commit('loginUser');
+    }
+  },
+  getters: {
 
   }
 })
