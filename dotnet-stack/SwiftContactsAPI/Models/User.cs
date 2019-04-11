194a4ef953112fec51f1ce5c24d4ac55911c1258
jingle1000 <jingle1000@gmail.com>
2019-04-10 21:20:19 -0400

    Add user entity for database

---
+++ dotnet-stack/SwiftContactsAPI/Models/User.cs
@@ -0,0 +1,9 @@
+namespace SwiftContactsAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
